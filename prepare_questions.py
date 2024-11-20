57cbe87b8851d6b467053a164b12d2426722c1af
a-skripko <skripkoae@gmail.com>
2024-11-20 12:12:01 +0400

    script prepare json file with questions from a list of files in folder

--- prepare_questions.py
+++ prepare_questions.py
@@ -1,7 +1,11 @@
+import json
+from pathlib import Path
+
 import chardet
+from environs import Env
 
 
-def open_qestions(filename: str) -> str:
+def open_questions(filename: str) -> str:
     with open(f"{filename}", "rb") as raw:
         encode_type = chardet.detect(raw.read(1000))["encoding"]
 
@@ -10,21 +14,38 @@ def open_qestions(filename: str) -> str:
     return text
 
 
-def create_questions_answers(filename: str) -> dict:
+def create_questions_answers(files: list[str]) -> dict[str:str]:
     questions = []
     answers = []
-    text = open_qestions(filename)
-    text_splitted = [
-        paragraph.strip()
-        for paragraph in text.split("\n\n")
-        if paragraph.strip()
-    ]
-    for line in text_splitted:
-        if line.startswith("Вопрос"):
-            questions.append(line)
-        if line.startswith("Ответ"):
-            answers.append(line)
+    for filename in files:
+        text = open_questions(filename)
+        text_splitted = [
+            paragraph.strip()
+            for paragraph in text.split("\n\n")
+            if paragraph.strip()
+        ]
+        for line in text_splitted:
+            if line.startswith("Вопрос"):
+                questions.append(line)
+            if line.startswith("Ответ"):
+                answers.append(line)
     questions_and_answers = {
         question: answer for question, answer in zip(questions, answers)
     }
     return questions_and_answers
+
+
+def main():
+    env = Env()
+    env.read_env()
+    directory_path = Path(env("RAW_QUESTIONS_PATH"))
+    files = [file for file in directory_path.iterdir() if file.is_file()]
+    questions_and_answers = create_questions_answers(files)
+    with open("questions.json", "w", encoding="utf-8") as json_file:
+        json.dump(
+            questions_and_answers, json_file, ensure_ascii=False, indent=4
+        )
+
+
+if __name__ == "__main__":
+    main()
