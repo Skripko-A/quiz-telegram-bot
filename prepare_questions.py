9893fefc52437c944b49f49a7ff968043cd9027b
a-skripko <skripkoae@gmail.com>
2024-11-26 09:05:10 +0400

    readability

--- prepare_questions.py
+++ prepare_questions.py
@@ -6,43 +6,28 @@ import chardet
 from settings import settings
 
 
-def open_questions(filename: str) -> str:
+def build_question_answer_pairs(files: list[str]) -> dict[str, str]:
     """
-    Opens a file and reads it as text, detecting the encoding type of the file.
+    Creates a dictionary of questions and answers from a list of files.
+
+    The function reads each file, splits its content into paragraphs, and
+    then checks if each paragraph starts with "Вопрос" or "Ответ". If it
+    does, the function adds the paragraph to the list of questions or
+    answers, respectively. After all files have been processed, the function
+    creates a dictionary where each key is a question and its value is the
+    corresponding answer.
 
     Args:
-        filename (str): The name of the file to open.
+        files: A list of paths to the files to be processed.
 
     Returns:
-        str: The text from the file.
-    """
-    with open(f"{filename}", "rb") as raw:
-        encode_type: str = chardet.detect(raw.read(1000))["encoding"]
-
-    with open(f"{filename}", encoding=encode_type) as file:
-        text: str = file.read()
-    return text
-
-
-def create_questions_answers(files: list[str]) -> dict[str, str]:
-    """
-    Extracts questions and answers from a list of text files and returns them as a dictionary.
-
-    This function reads each file in the provided list, detects its encoding, and extracts
-    paragraphs of text. It identifies and separates questions and answers based on specific
-    keywords ("Вопрос" and "Ответ") at the beginning of each paragraph. The function then
-    pairs each question with its corresponding answer in a dictionary.
-
-    Args:
-        files (list[str]): A list of filenames containing questions and answers.
-
-    Returns:
-        dict[str, str]: A dictionary where each key is a question (str) and its value is the corresponding answer (str).
+        A dictionary of questions and answers.
     """
     questions: list[str] = []
     answers: list[str] = []
     for filename in files:
-        text: str = open_questions(filename)
+        with open(f"{filename}", encoding="KOI8-R") as file:
+            text: str = file.read()
         text_splitted: list[str] = [
             paragraph.strip()
             for paragraph in text.split("\n\n")
@@ -87,7 +72,7 @@ def main() -> None:
     files: list[Path] = [
         file for file in directory_path.iterdir() if file.is_file()
     ]
-    questions_and_answers: dict[str, str] = create_questions_answers(files)
+    questions_and_answers: dict[str, str] = build_question_answer_pairs(files)
     with open("questions.json", "w", encoding="utf-8") as json_file:
         json.dump(
             questions_and_answers, json_file, ensure_ascii=False, indent=4
